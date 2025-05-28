4e4fb58039cd3c86200ae76f80081917c2174b08
daniel <danma1696@gmail.com>
2025-05-28 21:58:02 +0200

    matrices.py

--- Backend/FastApi/users.py
+++ Backend/FastApi/users.py
@@ -7,13 +7,14 @@ app = FastAPI()
 
 # Entidad user
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
     
-users_list = [User(name="Brais", surname="Moure", url="https:// moure.dev", age=35 ), 
-              User(name="Moure", surname="Dev", url="https://mouredev.com", age=27), User(name="Dashlberg", surname="Haakon", url="https://Hankoon.com", age=35) ]
+users_list = [User(id=1, name="Brais", surname="Moure", url="https:// moure.dev", age=35 ), 
+              User(id=2, name="Moure", surname="Dev", url="https://mouredev.com", age=27), User(id=3, name="Dashlberg", surname="Haakon", url="https://Hankoon.com", age=35) ]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -26,4 +27,23 @@ async def usersjson():
 async def users():
     return users_list
 
-# 2:05
+# Path
+
+@app.get("/user/{id}")
+async def user(id:int):
+    return search_user(id)
+
+# Query
+
+@app.get("/user/")
+async def user(id: int):
+    return search_user(id)
+    
+def search_user(id:int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"Error": "no encontrado  el usuario"}
+    
+# 2:22
